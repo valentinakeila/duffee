33f9fa297009808fee005235c7340925ace9e5ba
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-06-23 20:39:19 -0300

    revertir cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,60 @@
-import { useState } from 'react';
 import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 import Landing from './pages/landing/Landing'
 import LoginPage from './pages/login/LoginPage';
-import PageNotFound from './components/pageNotFound/PageNotFound'
-import ProtectedRoutesUser from './components/protectedRoutes/ProtectedRoutesUser';
-import ProtectedRoutesAdmin from './components/protectedRoutes/ProtectedRoutesAdmin';
+import Menu from './pages/menu/Menu';
+
+import MenuAdmin from "./pages/menuAdmin/MenuAdmin"
+import { useState } from 'react';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(isLoggedIn);
-  const [isAdmin, setIsAdmin] = useState(false);
-  console.log(isAdmin);
-
-  const loginHandler = () => {
-    //seguramente tengamos que modificar este if para que cambie el estado
-    //sólo si el usuario existe en la base de datos y el response retorna un 200
-    if (!isLoggedIn) {
-      setIsLoggedIn(true);
+  const [menuCategories,setMenuCategories] = useState([
+    {
+      name:"Desayunos",
+      imageUrl:"https://images.unsplash.com/photo-1584450149783-8073785c47f9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id:Math.random().toString(36).substr(2, 9)
+    },
+    {
+      name:"Almuerzo",
+      imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTFlYjOZNwya2KTfdOk8HYyQRJVapkrQY-xIN1HLRIvxcIkdwYj8TBj-FNumqlyioAF9w&usqp=CAU"
+      ,
+      id:Math.random().toString(36).substr(2, 9)
+    },
+    {
+      name:"Cafeteria",
+      imageUrl:"https://images.unsplash.com/photo-1494415859740-21e878dd929d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id:Math.random().toString(36).substr(2, 9)
+    },
+    {
+      name:"Postres",
+      imageUrl:"https://images.unsplash.com/photo-1561845730-208ad5910553?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id:Math.random().toString(36).substr(2, 9)
+    },
+    {
+      name:"Bebidas",
+      imageUrl:"https://images.unsplash.com/photo-1614432807303-16ff46aefc8e?q=80&w=1486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id:Math.random().toString(36).substr(2, 9)
+    },
+    {
+      name:"Para compartir",
+      imageUrl:"https://i.ytimg.com/vi/XCENOkjqI8c/maxresdefault.jpg",
+      id:Math.random().toString(36).substr(2, 9)
     }
-
-    /*
-      Aquí va un if... para cuando nos llega la response poder validar si el usuario
-      logueado es Admin o no, para así poder setear a true el estado de isAdmin si
-      es necesario   
-    */
-
-  };
+  ])
 
   const router = createBrowserRouter([
-    //rutas públicas
     { path: "/", element: <Landing /> },
-    { path: "/login", element: <LoginPage onLogin={loginHandler}/> },
-    { path: "*", element: <PageNotFound/> },
-    //rutas privadas...(renombrar según corresponda)
-    { path: "/rutaadmin", element: <ProtectedRoutesAdmin isSignedIn={isLoggedIn} isAdmin={isAdmin}/>,
-      children: [
-        {
-          //hacer coincidir esta ruta con la del componente ProtectedRoutesAdmin
-          path: "/rutaadmin", element: "Componente de Menu de Admin"
-        },
-        //agregar aquí las rutas anidadas de Admin
-      ]
-    },
-    { path: "/rutauser", element: <ProtectedRoutesUser isSignedIn={isLoggedIn} isAdmin={isAdmin}/>,
-      children: [
-        {
-          //hacer coincidir esta ruta con la del componente ProtectedRoutesUser
-          path: "/rutauser", element: "Componente de Menu de Usuario"
-        },
-        //Agregar aquí las rutas anidadas de User
-      ]
-    },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/menu", element: <Menu menuCategories={menuCategories}/> },
+    //more pages...
+    //{ path: "*", element: <PageNotFound/> }
+
+    { path: "/menuAdmin", element: <MenuAdmin menuCategories={menuCategories}  setMenuCategories={setMenuCategories}/> },
   ]);
 
   return <RouterProvider router={router} />
+
 }
 
 export default App
