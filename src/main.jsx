89c070b90156d579920905d298f93a9b26533e5e
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-12 13:29:14 -0300

    Add ShoppingCartContextProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { AuthenticationContextProvider } from './components/services/authentication/UserAuthenticationContext.jsx'
+import { ShoppingCartContextProvider } from './components/services/shoppingCartContext/UserShoppingCartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-	<AuthenticationContextProvider>
-    	<App />
+	  <AuthenticationContextProvider>
+      <ShoppingCartContextProvider>
+        <App />
+      </ShoppingCartContextProvider>
     </AuthenticationContextProvider>
   </React.StrictMode>,
 )
