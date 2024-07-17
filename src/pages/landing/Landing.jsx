85eaf88dfd534e9c886f8dbae16aa84ec3c7e9fe
Nicolas Cataldi <nicolascataldi1@gmail.com>
2024-07-17 03:35:19 -0300

    Cambios en landing

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -20,7 +20,7 @@ function Landing() {
     <>
       {currentUser ? <NavBarUser /> : <Navigation />}
       <div className="d-flex mt-1" style={{position:"absolute",zIndex:2,marginLeft:"2rem"}}>
-        <button onClick={handleLanguageSwitch} className="mt-5 fs-5 text-white rounded py-2 fw-bold" style={{backgroundColor:"#FFC091",color:"#FFFFFF",border:"solid 1px white"}}>
+        <button onClick={handleLanguageSwitch} className="mt-5 fs-5 text-white rounded py-2 px-1 fw-bold" style={{backgroundColor:"#FFC091",color:"#FFFFFF",border:"solid 1px white"}}>
           {isLatin ? "Cambiar a Español" : "Translate to Latin"}
         </button>
       </div>
